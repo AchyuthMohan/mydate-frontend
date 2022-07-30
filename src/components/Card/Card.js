da238df88c58819ac3e95d23cce4f79f29ecc9e1
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-07-30 08:56:17 +0530

    initial commit

---
+++ src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Card.css'
+const Card = (props) => {
+  return (
+    <div>
+        <div class="card">
+    <div class="icon">
+      <ion-icon name="globe-outline"></ion-icon>
+    </div>
+    <div class="content">
+      <h2>To: {props.name}</h2>
+      <p>Message: {props.message}</p>
+      <p>author: {props.auth_name}</p>
+      <p>Email: {props.email}</p>
+      <p>Author Phone: {props.phno}</p>
+      <button>remove</button>
+      
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react'
+import './MainPage.css'
+import Lottie from 'react-lottie'
+import anim from '../../animations/anim.json'
+import Card from '../../components/Card/Card'
+import { baseUrl } from '../../utils/urls'
+import axios from 'axios'
+const MainPage = () => {
+    const[persons,setPerson]=useState([])
+    const[crushName,setCrushName]=useState('')
+    const[yourName,setYourName]=useState('')
+    const[email,setEmail]=useState('')
+    const[pno,setPno]=useState(0)
+    const[message,setMessage]=useState('')
+
+
+    useEffect(()=>{
+        axios.get(`${baseUrl}/person/`).then((response)=>{
+            setPerson(response.data)
+        },(error)=>{
+            console.log(error)
+        })
+    },[])
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        axios.POST(`http://127.0.0.1:8000/person/`,{
+            email:email,
+            name:crushName,
+            auth_name:yourName,
+            message:message,
+            auth_phno:pno,
+
+        }).then((response)=>{
+            console.log(response)
+        },(error)=>{
+            console.log(error)
+        })
+    }
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: anim,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+  return (
+    <div>
+        <div className="parent__container">
+        <div class="container">  
+  
+</div>
+            <div className="title">My Dater</div>
+            <div className="land__anim">
+                <Lottie options={defaultOptions} style={{width:'90%' ,height:'30%'}}/>
+            </div>
+
+            <form id="contact" onSubmit={handleSubmit}>
+    <fieldset>
+      <input value={crushName} placeholder="Your crush name" type="text" tabindex="1" required autofocus onChange={(e)=>{setCrushName(e.target.value)}}/>
+    </fieldset>
+    <fieldset>
+      <input value={yourName} placeholder="Your  name" type="text" tabindex="1" required autofocus onChange={(e)=>{setYourName(e.target.value)}}/>
+    </fieldset>
+    <fieldset>
+      <input value={email} placeholder="Your Email Address" type="email" tabindex="2" required onChange={(e)=>{setEmail(e.target.value)}}/>
+    </fieldset>
+    <fieldset>
+      <input value={pno} placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required onChange={(e)=>{setPno(e.target.value)}}/>
+    </fieldset>
+    <fieldset>
+      <textarea value={message} placeholder="Type your message here...." tabindex="5" required onChange={(e)=>{setMessage(e.target.value)}}></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Send</button>
+    </fieldset>
+   
+  </form>
+
+
+            <div className="child__container">
+                {persons.map((person)=>{
+                    return(
+                    <Card id={person.id} name={person.name} message={person.message} auth_name={person.author_name} email={person.email} phno={person.auth_phno} data={person.date} />
+                    )
+                })}
+
+            </div>
+        </div>
+
+        <div id='stars'></div>
+            <div id='stars2'></div>
+            <div id='stars3'></div>
+    </div>
+  )
+}
+
+export default MainPage
